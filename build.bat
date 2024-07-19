9374031e0cfd310b5a026a2fc67d77f73cf7c09c
Caitex212 <firewizard787@gmail.com>
2024-07-19 15:38:50 +0200

    fixed build.bat for icon

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller -w --onefile --add-data="sounds;sounds" --icon=icon.ico --name "Project_Slotcar" main.py
+pyinstaller -w --onefile --add-data="sounds;sounds" --icon=app.ico --name "Project_Slotcar" main.py
